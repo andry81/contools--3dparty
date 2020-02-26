acc64f5f60887a0ff7eeedcec8ca1aede07b44bb
andry81 <andry@inbox.ru>
2020-02-26 22:48:33 +0000

    2020.02.27: * new: `__init__.bat` script port into `__init__.sh` script * new: `configure.bat` script port into `configure.sh` script * changed: missed change
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/3dparty/trunk@42 d0ebecaf-9e5c-43b6-8085-e2620c37aada

---
+++ configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script ONLY for execution.
+if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then 
+
+source "/bin/bash_entry" || exit $?
+tkl_include "__init__.sh" || exit $?
+
+{
+  cat "$CONFIGURE_ROOT/environment.vars.in"
+} > "$CONFIGURE_ROOT/environment.vars"
+
+fi
